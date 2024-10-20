cd148ffe0300f7eef923e3cd2f512cc0407a4854
Alexandru Iulian Gheorghita <hamuxxx@gmail.com>
2024-10-21 00:12:39 +0200

    :sparkles: added a simple list to add books

--- index.js
+++ index.js
@@ -11,21 +11,61 @@ function Book(title, author, pagesAmount, read) {
     }
 }
 
+const display = document.getElementById("display")
+
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    displayBooks();
 }
 
-function showBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i].title)
-    }
+function displayBooks() {
+
+    const booksList = document.createElement("div");
+    booksList.classList.add("books-list");
+    display.appendChild(booksList);
+
+
+    myLibrary.forEach((book) => {
+        const bookItem = document.createElement("div");
+        bookItem.classList.add("book-item");
+        bookItem.textContent = book.info();
+        booksList.appendChild(bookItem);
+    })
+
+    display.appendChild(booksList);
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
-const hobbit1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
-const hobbit2 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
-addBookToLibrary(hobbit1)
-addBookToLibrary(hobbit2)
-showBooks()
+function addBook(title, author, pagesAmount) {
+    return new Book(title, author, pagesAmount, false);
+}
+
+const dialog = document.querySelector("dialog");
+const button = document.getElementById("add");
+const form = document.querySelector("form");
+
+button.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const addButton = document.getElementById("addBook");
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pagesAmount = document.getElementById("pagesAmount").value;
+    addBookToLibrary(addBook(title, author, pagesAmount));
+
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pagesAmount").value = "";
+})
+
+const closeButton = document.getElementById("close");
+closeButton.addEventListener("click", () => {
+    dialog.close();
+})
+
 
 
